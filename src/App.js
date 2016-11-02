f76868157586b3ffc61807e080dce7e51d8cea19
Jurie Horneman <jurie@jurie.org>
2016-11-02 23:12:14 +0100

    Loading data from Google Spreadsheet.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import Tabletop from 'tabletop';
 import './App.css';
 
 
+function loadDataFromGoogleSpreadsheet() {
+    Tabletop.init({
+        key: '1AHirIj1eUn8ofGamkkCgu2XWXweM4byqrcu9FAg-j48',
+        callback: dataFromGoogleSpreadsheetHasLoaded,
+        simpleSheet: true
+    });
+}
+
+function dataFromGoogleSpreadsheetHasLoaded(data, tabletop) {
+    console.log(data);
+}
+
 function loadQuests() {
-    // fetch('/data/JungleQuests.xlsx')
-    // .then(function(response) {
-    //     return response.arrayBuffer();
-    // }).then(function(arrayBuffer) {
-    //     parseQuests(arrayBuffer);
-    // });
+    if (process.env.NODE_ENV === 'development') {
+        loadDataFromGoogleSpreadsheet();        
+    } else {
+        // loadDataFromJSONFile();
+    }
 }
 
 
