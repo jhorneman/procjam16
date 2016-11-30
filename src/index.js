1cc02ee83dfc7136f09ac348c337a4fbc2aaa3ed
Jurie Horneman <jurie@jurie.org>
2016-11-30 13:37:20 +0100

    Loading background images before starting the game.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { loadImages } from './images';
 import { GameStoreMutator } from './gameStore';
 
 import 'normalize.css';
 import './main.css';
 
 
-GameStoreMutator.init();
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+loadImages().then(function() {
+    GameStoreMutator.init();
+    ReactDOM.render(
+      <App />,
+      document.getElementById('root')
+    );
+});
